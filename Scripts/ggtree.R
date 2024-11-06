72e311e0bc1679d7bfdbfb8c21afcd75aa023c6d
Daniela Villanueva <dvillanueva16@alumnos.uaq.mx>
2024-11-06 10:32:41 -0600

    xlim

--- Scripts/ggtree.R
+++ Scripts/ggtree.R
@@ -21,5 +21,8 @@ ggtree (arbol_clustal, branch.length = "none")+
 ggtree(arbol_clustal, layout = "circular", branch.length = "none") +
   geom_tiplab(size = 2, offset = 0.1)   
 
+ggtree(arbol_clustal, branch.length = "none") +
+  geom_tiplab(size = 3, offset = 0.1, color = "red") + 
+  xlim(NA, 5) ##De esta manera pude reducir las ramas 
 
   
